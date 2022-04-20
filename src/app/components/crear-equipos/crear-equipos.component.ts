c6b65f03d8aa0741db03dfd50c946f31091aaf5b
Jessica Trevino <jessy.trevi00@gmail.com>
2022-04-20 17:38:14 -0500

    upload file (front) works! sin estilo

--- src/app/components/crear-equipos/crear-equipos.component.ts
+++ src/app/components/crear-equipos/crear-equipos.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+
+@ViewChild('takeInput', {static: false})
 
 @Component({
   selector: 'app-crear-equipos',
   templateUrl: './crear-equipos.component.html',
   styleUrls: ['./crear-equipos.component.css']
 })
+
 export class CrearEquiposComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+ 
+
+  onFileChange(evt: any) {
+    const target : DataTransfer = <DataTransfer>(evt.target.files);
+    if (target.files.length !== 1) throw new Error("Cannot use multiple files!");
   }
 
 }
