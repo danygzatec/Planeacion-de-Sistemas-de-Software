86467b19ebd52498d17f23a379a476164867535c
melissamtzm <a00827008@itesm.mx>
2022-04-21 00:24:36 -0500

    Navbar bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MsalService } from '@azure/msal-angular';
 import { AuthenticationResult } from '@azure/msal-browser';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MsalModule, MsalService, MSAL_INSTANCE } from '@azure/msal-angular';
 import { IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
+
 export function MSALInstanceFactory() : IPublicClientApplication{
   return new PublicClientApplication({
     auth : {
