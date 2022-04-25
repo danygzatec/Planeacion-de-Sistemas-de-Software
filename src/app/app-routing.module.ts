c2d6b79711cbc460c5cfe0700ca48189eab3f3fc
melissamtzm <a00827008@itesm.mx>
2022-04-24 20:20:09 -0500

    Inbox en Navbar, Bottom border, Navbar de Consult Teams

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { Evaluacion360Component } from '../app/components/evaluacion360/evaluaci
 import { RouterModule, Routes } from '@angular/router';
 import { MsalguardGuard } from './msalguard.guard';
 import { MsalGuard } from '@azure/msal-angular';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent, canActivate: [MsalguardGuard]},
   {path: 'crear-equipos', component: CrearEquiposComponent, canActivate: [MsalguardGuard]},
   {path: 'consultar-equipos', component: ConsultarEquiposComponent, canActivate: [MsalguardGuard]},
   {path: 'evaluacion360', component: Evaluacion360Component, canActivate: [MsalguardGuard]},
+  {path: 'dashboard', component: DashboardComponent, canActivate: [MsalguardGuard]},
   {path: '', pathMatch: 'full', redirectTo: 'home'}
 ];
 
@@ -20,4 +22,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [HomeComponent, CrearEquiposComponent, ConsultarEquiposComponent, Evaluacion360Component]
+export const routingComponents = [HomeComponent, CrearEquiposComponent, ConsultarEquiposComponent, Evaluacion360Component, DashboardComponent]
