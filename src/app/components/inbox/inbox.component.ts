296f3ea7d85dfc195e508f85faab5579b68e82e9
Emma Gomez <a00827097@itesm.mx>
2022-05-03 15:49:18 -0500

    inbox no sirve

--- src/app/components/inbox/inbox.component.ts
+++ src/app/components/inbox/inbox.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee';
+import { Request } from 'src/app/models/request'
+import ExcelData from 'src/excel-dummy.json';
 
 @Component({
   selector: 'app-inbox',
@@ -6,10 +9,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inbox.component.css']
 })
 export class InboxComponent implements OnInit {
+  public request: Request[];
+  public employee: Employee[];
 
-  constructor() { }
+  constructor() {
+    this.request = ExcelData.request;
+    this.employee = ExcelData.employee;
+   }
 
-  ngOnInit(): void {
+   async initializaObjects() {
+     this.request = ExcelData.request;
+     this.employee = ExcelData.employee;
+   }
+
+  async ngOnInit(): Promise<void> {
+    await this.initializaObjects();
   }
 
+  getRequest() {
+    return this.request;
+  }
+
+  getTitle() : string[] {
+    var title: string[] = [];
+    var currString : string = "";
+
+    this.request.forEach(req => {
+      var e = this.employee.find(emp => emp.id_employee == req.id_emp_req);
+      currString.concat(e!.employee_name);
+      currString.concat("wants to ");
+      if(req.type == 0){
+        currString.concat( " add ");
+      } else if(req.type == 1){
+        currString.concat( " remove ");
+      }
+
+      e = this.employee.find(emp => emp.id_employee == req.id_emp_mod);
+
+      currString.concat(e!.employee_name);
+      console.log(currString.toString());
+      title.push(currString);
+      currString = "";
+      
+    });
+
+    return title;
+  }
 }
