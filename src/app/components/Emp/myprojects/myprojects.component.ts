342a098fd02a3238b85ae35999d3d6a2e4801037
Jessica Trevino <jessy.trevi00@gmail.com>
2022-05-19 00:10:54 -0500

    myprojects.ts commented

--- src/app/components/Emp/myprojects/myprojects.component.ts
+++ src/app/components/Emp/myprojects/myprojects.component.ts
@@ -12,34 +12,38 @@ import { ChartData, ChartType } from 'chart.js';
   styleUrls: ['./myprojects.component.css']
 })
 export class MyprojectsComponent implements OnInit {
-  projects : Project[];
-  employee : Employee[];
-  empProject : EmployeeProject[];
-  billableHours : number[];
-  nonBillableHours : number[];
+  projects: Project[];
+  employee: Employee[];
+  empProject: EmployeeProject[];
+  billableHours: number[];
+  nonBillableHours: number[];
 
   searchText: any;
 
-  // chart
-
-  public doughnutChartDataB : ChartData<'doughnut'> = {
+  // doughnut chart
+  // billable
+  public doughnutChartDataB: ChartData<'doughnut'> = {
     labels: [],
     datasets: [
-      { data: [ ] }
+      { data: [] }
     ]
   };
-  public doughnutChartDataNB : ChartData<'doughnut'> = {
+  // non-billable
+  public doughnutChartDataNB: ChartData<'doughnut'> = {
     labels: [],
     datasets: [
-      { data: [ ] }
+      { data: [] }
     ]
   };
+  // decimos que el chart type es tipo doughnut
   public doughnutChartType: ChartType = 'doughnut';
 
-  constructor(public accountInfo: AppComponent) { 
+  constructor(public accountInfo: AppComponent) {
     this.projects = ExcelData.project;
     this.employee = ExcelData.employee;
     this.empProject = ExcelData.employee_project;
+
+    // tenemos que hacer esto para que los doughnut charts muestren datos
     this.billableHours = this.getBillableHours();
     this.nonBillableHours = this.getNonBillableHours();
 
@@ -52,7 +56,11 @@ export class MyprojectsComponent implements OnInit {
     await this.createObjects();
   }
 
+  /* 
+  En esta función creamos los objetos que están dentro de cada arreglo de Interfaces
+  */
   async createObjects() {
+
     this.projects = ExcelData.project;
     this.employee = ExcelData.employee;
     this.empProject = ExcelData.employee_project;
@@ -66,13 +74,19 @@ export class MyprojectsComponent implements OnInit {
   }
 
   getProjects() {
-    var userProjects : Project[] = [];
+    var userProjects: Project[] = [];
+
+    // buscamos al empleado que está signed in
     var e = this.employee.find(emp => emp.employee_name === this.accountInfo.getNameAccount());
+
+    // buscamos los proyectos en los que trabajó y que completó las horas
     this.empProject.forEach(element => {
       if (element.did_complete && element.id_employee === e!.id_employee) {
         var id = element.id_project;
         var proj = this.projects.find(pjct => pjct.id_project === id);
         userProjects.push(proj!);
+
+        // asignar las labels a los doughnut charts
         this.doughnutChartDataB.labels!.push(proj!.project_name);
         this.doughnutChartDataNB.labels!.push(proj!.project_name);
       }
@@ -83,12 +97,16 @@ export class MyprojectsComponent implements OnInit {
 
   getBillableHours() {
 
-    var billHours : number[] = [];
+    var billHours: number[] = [];
 
+    // encontramos el empleado que está signed in
     var e = this.employee.find(emp => emp.employee_name === this.accountInfo.getNameAccount());
+
+    // buscamos los proyectos en los que trabajó
     this.empProject.forEach(element => {
       if (element.id_employee === e!.id_employee) {
         billHours.push(element.billableHours);
+        // asignamos valores en el dataset correspondiente de nuestro chart
         this.doughnutChartDataB.datasets[0].data.push(element.billableHours);
       }
     })
@@ -98,12 +116,17 @@ export class MyprojectsComponent implements OnInit {
   }
 
   getNonBillableHours() {
-    var nonBillHours : any[] = [];
 
+    var nonBillHours: any[] = [];
+
+    // encontramos el empleado que está signed in
     var e = this.employee.find(emp => emp.employee_name === this.accountInfo.getNameAccount());
+
+    // buscamos los proyectos en los que trabajó
     this.empProject.forEach(element => {
       if (element.id_employee === e!.id_employee) {
         nonBillHours.push(element.nonBillableHours);
+        // asignamos valores en el dataset correspondiente de nuestro chart
         this.doughnutChartDataNB.datasets[0].data.push(element.nonBillableHours);
       }
     })
@@ -111,6 +134,6 @@ export class MyprojectsComponent implements OnInit {
     return nonBillHours;
 
   }
-  
+
 
 }
