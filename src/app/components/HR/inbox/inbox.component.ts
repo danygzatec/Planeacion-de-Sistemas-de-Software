fc9fd8a5ea5930c86fb09754f8619ad48cf73233
Emma Gomez <a00827097@itesm.mx>
2022-06-06 16:15:20 -0500

    rol

--- src/app/components/HR/inbox/inbox.component.ts
+++ src/app/components/HR/inbox/inbox.component.ts
@@ -77,11 +77,11 @@ export class InboxComponent implements OnInit {
   }
 
   countRequests(): number {
-    this.sql.getRequests().subscribe((resp) => {
-      this.request = resp;
-      this.lastRequestLength = resp.length;
-    })
-    console.log("count req",this.request);
+  //   this.sql.getRequests().subscribe((resp) => {
+  //     this.request = resp;
+  //     this.lastRequestLength = resp.length;
+  //   })
+  //   console.log("count req",this.request);
     return this.request.length;
   }
 
